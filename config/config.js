db356efab0046c10596b41b477c4a90c10f9fc6f
Brent Gustafson <50030696+BrentG6000@users.noreply.github.com>
2021-06-03 12:19:30 -0500

    Delete config directory

--- config/config.js
+++
@@ -1,8 +0,0 @@
-//const TwitterStrategy = require('passport-twitter').Strategy
-
-module.exports = {  
-    consumer_key: '181SpVoBdQ8DmLgq6uKR576Gs' ,  
-    consumer_secret: 'UEHmiIDcnLrDsB2Y4E56ibdSTJR3AOfjy60fFRqJIW9jMZXsgb',  
-    access_token_key: '',  
-    access_token_secret: ''  
-}
