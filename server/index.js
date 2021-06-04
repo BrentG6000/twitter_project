3f7334e73bf36cab59e8496edb8512505240f827
McNasty6000 <brentg123@gmail.com>
2021-06-04 02:31:16 -0500

    Debuging

--- server/index.js
+++ server/index.js
@@ -98,10 +98,15 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(express.json())
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
 app.get('/auth/twitter', passport.authenticate('twitter'))
 app.get('/auth/twitter/callback', 
   passport.authenticate('twitter', { failureRedirect: '/login' }),
   function(req, res) {
+      //res.redirect('http://localhost:3000/Profile')
       res.redirect('https://brentg123-twitter-project.herokuapp.com/Profile')
   })
 app.get('/user', (req, res) => {
