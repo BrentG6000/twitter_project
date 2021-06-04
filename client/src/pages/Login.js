47004cfb0db27caf382f3c4167a3c3297709ef7b
McNasty6000 <brentg123@gmail.com>
2021-06-04 00:24:23 -0500

    Changed buttonLink for profile page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -6,7 +6,7 @@ function Login() {
     const bodyText = 'This link will allow you to link your Twitter account to begin logging tweets.'
     const buttonText = 'Sign in'
     const addButton = 'true'
-    const buttonLink = 'http://localhost:5000/auth/twitter'
+    const buttonLink = 'https://brentg123-twitter-project.herokuapp.com/auth/twitter'
     return (
         <div id='resize'>
         <Frost headerText={headerText} bodyText={bodyText} buttonText={buttonText} addButton={addButton} buttonLink={buttonLink}/>
