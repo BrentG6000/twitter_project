e4bec1313ac0da44710913f79d5fd24bb6a29021
McNasty6000 <brentg123@gmail.com>
2021-06-14 01:59:33 -0500

    Fixed font for mobile profile page

--- server/index.js
+++ server/index.js
@@ -60,8 +60,8 @@ app.get('/auth/twitter', passport.authenticate('twitter'))
 app.get('/auth/twitter/callback', 
   passport.authenticate('twitter', { failureRedirect: '/login' }),
   function(req, res) {
-      res.redirect('http://localhost:3000/Profile')
-      //res.redirect('https://brentg123-twitter-project.herokuapp.com/Profile')
+      //res.redirect('http://localhost:3000/Profile')
+      res.redirect('https://brentg123-twitter-project.herokuapp.com/Profile')
   })
 app.get('/user', (req, res) => {
     res.send(user)
